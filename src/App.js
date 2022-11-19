b24b860622366ba341ddb0d6e64cff7a4712ac2e
Dragoh54 <ndragoh@gmail.com>
2022-11-19 17:09:33 +0300

    redirect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
+  redirect,
   RouterProvider,
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Layout from "./routes/Layout";
 import Login from "./routes/Login";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="*" element={<div>NOT FOUND</div>} />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    loader: () => {
+      const user = localStorage.getItem("user");
+      if (!user) {
+        return redirect("/login");
+      }
+    },
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "*",
+        element: <h1>Not Found!</h1>,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
